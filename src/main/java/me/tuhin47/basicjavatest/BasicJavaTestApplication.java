25f026da2266a085aee5d27504ba22803237e50e
MD. Towhidul Islam <tuhintowhidul9@gmail.com>
2021-04-06 18:43:11 +0600

    git init

---
+++ src/main/java/me/tuhin47/basicjavatest/BasicJavaTestApplication.java
@@ -0,0 +1,13 @@
+package me.tuhin47.basicjavatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicJavaTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicJavaTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/tuhin47/basicjavatest/BasicJavaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package me.tuhin47.basicjavatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicJavaTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
